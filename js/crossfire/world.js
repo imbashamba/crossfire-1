51a6a185842053bf76fa6477bb7e9f06cc7aa154
Pavel Lonkin <lonkin01@gmail.com>
2013-08-16 15:34:54 +0400

    Add CanvasElement object

--- js/crossfire/world.js
+++ js/crossfire/world.js
@@ -3,8 +3,37 @@
  *
  * @author lonkin01@gmail.com (Lonkin Pavel)
  */
-(function() {
+(function(global) {
     "use strict";
 
-    
-}());
+    /**
+     Canvas elemen
+     */
+    var CanvasElem = {
+
+        canvas: null,
+
+        context: null,
+
+        width: null,
+        height: null,
+
+        /**
+         * Canvas initialization
+         * 
+         * @param canvasId {String} id of canvas in DOM
+         */
+        initialize: function(canvasId) {
+            this.canvas = document.getElementById(canvasId),
+            this.context = this.canvas.getContext('2d');
+            this.width = this.canvas.width-0;
+            this.height = this.canvas.height-0;
+        }
+    };
+
+    if(!global.cf) {
+        global.cf = {};
+    }
+    global.cf.CanvasElem = CanvasElem;
+
+}(this));
