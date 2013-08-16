f866176ba7732ce0cd03ca7ff3980ec103f35f7e
Pavel Lonkin <lonkin01@gmail.com>
2013-08-16 16:36:16 +0400

    Add World object with a function of initialization and updating the world

--- js/crossfire/world.js
+++ js/crossfire/world.js
@@ -31,9 +31,71 @@
         }
     };
 
+    /**
+     b2World
+     */
+    var World = {
+
+        /**
+         * Constant, because Box2D unite is meter
+         */
+        SCALE: 30,
+
+        world: null,
+
+        canvas: null,
+
+        borders: {},
+
+        /**
+         * Create world
+         */
+        initialize: function() {
+            var gravity = new b2Vec2(0, 0),
+                worldAABB = new b2AABB();
+
+            this.canvas = CanvasElem.initialize('canvas');
+
+            worldAABB.minVertex.Set(-1000, -1000);
+            worldAABB.maxVertex.Set(1000, 1000);
+
+            this.world = new b2World(worldAABB, gravity, true);
+
+            this.createBorders();
+
+            this.update();
+        },
+
+        /**
+         * Setting timer for updating the world
+         */
+        update: function() {
+            this.step();
+
+            console.log("Updated");
+
+            setTimeout(this.update.bind(this), 10);
+        },
+
+        /**
+         * Iteration of our world
+         */
+        step: function() {
+            this.world.Step(1.0/60.0, 10, 10);
+        },
+
+        /**
+         * Create four borders: up, down, right, left
+         */
+        createBorders: function() {
+
+        }
+    };
+
     if(!global.cf) {
         global.cf = {};
     }
     global.cf.CanvasElem = CanvasElem;
+    global.cf.World = World;
 
 }(this));
